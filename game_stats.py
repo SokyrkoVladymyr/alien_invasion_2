10c3b89b03460161a94c68bd20bdd2b0eca5cf42
SokyrkoVladymyr <Sokyrko_V@ukr.net>
2022-05-04 17:46:02 +0300

    Final version of game alien invation

---
+++ game_stats.py
@@ -0,0 +1,17 @@
+
+class GameStats():
+
+    def __init__(self, ai_game):
+        """Инициализирует статистику."""
+        self.settings = ai_game.settings
+        self.reset_stats()
+        #Игра запускается в неактивном состоянии
+        self.game_active = True
+        self.high_score = 0
+        self.level = 1
+
+
+    def reset_stats(self):
+        """Инициализирует статистику, изменяющуюся в ходе игры."""
+        self.ships_left = self.settings.ship_limit
+        self.score = 0
--- ship.py
+++ ship.py
@@ -1,10 +1,13 @@
 import pygame
+from pygame.sprite import Sprite
 
-class Ship():
+class Ship(Sprite):
     """Класс для управления кораблем."""
 
     def __init__(self, ai_game):
+        super().__init__()
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # Загружает изображение корабля и получает прямоугольник.
@@ -14,9 +17,31 @@ class Ship():
         # Каждый новый корабль появляется у нижнего края экрана.
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Сохранение вещественной координаты центра корабля.
+        self.x = float(self.rect.x)
+
+        #флаг перемещения корабля
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """Обновляет позицию корабля с учетом флага."""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        elif self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+        # Обновление атрибута rect на основании self.x.
+        self.rect.x = self.x
+
+
     def blitme(self):
         """Рисует корабль в текущей позиции."""
         self.screen.blit(self.image, self.rect)
+
+    def center_ship(self):
+        """Размещает корабль в центре нижней стороны."""
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float (self.rect.x)
 
 
 
